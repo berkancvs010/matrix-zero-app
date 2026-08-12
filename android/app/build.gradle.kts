5519f493c732d34e3a9f88bdf82958cb06952c79
Berkan Çavuşoğlu <berkancvs010@users.noreply.github.com>
2026-08-12 08:42:35 +0000

    Update Android build configuration for SDK 37

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.matrix_zero"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
