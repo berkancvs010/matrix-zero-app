82481a68bd9ce893df0563f71f09733753e8e27e
Berkan Çavuşoğlu <berkancvs010@users.noreply.github.com>
2026-08-13 16:40:55 +0000

    Add call ringtone message sound and vibration

--- android/app/src/main/kotlin/com/zerolog/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/zerolog/app/MainActivity.kt
@@ -1,5 +1,38 @@
 package com.zerolog.app
 
+import android.media.RingtoneManager
+import android.net.Uri
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    private val channelName = "zerolog/system"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            channelName
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "getDefaultRingtoneUri" -> {
+                    try {
+                        val uri: Uri? = RingtoneManager.getActualDefaultRingtoneUri(
+                            this,
+                            RingtoneManager.TYPE_RINGTONE
+                        )
+
+                        result.success(uri?.toString())
+                    } catch (e: Exception) {
+                        result.success(null)
+                    }
+                }
+
+                else -> result.notImplemented()
+            }
+        }
+    }
+}
